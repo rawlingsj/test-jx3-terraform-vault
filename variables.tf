5fb0f13f94f4202c8bc92f7955d4a7b6fd5595ac
James Rawlings <rawlingsj80@gmail.com>
2020-08-11 15:07:47 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "jenkins_x_pipeline_bot_username" {
+  description = "Username used by pipelines to interact with git repositories"
+  type        = string
+}
+
+variable "jenkins_x_pipeline_bot_token" {
+  description = "Access Token used by pipelines to interact with git repositories"
+  type        = string
+}
